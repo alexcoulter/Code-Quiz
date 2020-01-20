4876024a949820e6fa5359a9b7d1b4905a843c46
Alex Coulter <ALEXCOULTER.UNC@GMAIL.COM>
2020-01-20 00:47:13 -0500

    first working project committed

---
+++ questions.js
@@ -0,0 +1,54 @@
+var questions = [
+  {
+    title: "Which of these coding languages is most useful for styling websites and applications?",
+    choices: ["CSS", "HTML", "Javascript", "Python"],
+    answer: "CSS"
+  },
+  {
+    title: "Which of these functions parses an argument and returns a floating point number:",
+    choices: ["numberify()", "parseFloat()", "parseInt()", "toNum()"],
+    answer: "parseFloat()"
+  },
+  {
+    title: "Which of these values would evalaute to false when it's boolean value is checked?",
+    choices: ["0", "null", "undefined", "All of the above"],
+    answer: "All of the above"
+  },
+  {
+    title: "What method would you use to turn a string into an array of substrings?",
+    choices: ["substr()", "split()", "slice()", "constructor()"],
+    answer: "split()"
+  },
+  {
+    title: "Which comparison operator would you use if you wanted to see if two variables have the same data types and values:",
+    choices: ["===", "==", "=", "!=="],
+    answer: "==="
+  },
+  {
+    title: "Which of these functions would execute every 5 seconds?",
+    choices: ["for(i = 0; i < 5sec; i++){}", "setTimeout(function(){}, 5000);", "for(i = 0; i < total; i = i + 5 sec){}", "setTimeout(function(){}, 5); "],
+    answer: "setTimeout(function(){}, 5000);"
+  },
+  {
+    title: "Which of these id's is written using camelcase:",
+    choices: ["#first-button", "#fIrStBuTtOn", "#first_BUTTON", "#firstButton"],
+    answer: "#firstButton"
+  },
+  {
+    title: "What is the world's most popular front-end component library?",
+    choices: ["Javascript", "JQuery", "Bootstrap", "Angular"],
+    answer: "Bootstrap"
+  },
+  {
+    title: "Which of these CSS rules would target all buttons with a class of 'hidden':",
+    choices: ["button('hidden'){}", "#hidden button{}", ".hidden button{}", "hidden.button"],
+    answer: ".hidden button{}"
+  },
+  {
+    title: "Which CSS declaration prioritizes a line of code and overrides inline styles:",
+    choices: ["&Amnesty", "*Special*", "$Priority", "!important"],
+    answer: "!important"
+  }
+];
+
+localStorage.setItem("questions", JSON.stringify(questions));
---
+++ script.js
@@ -0,0 +1,138 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+var startButton = document.getElementById("startQuiz");
+var timer = document.getElementById("timer");
+var h1content = document.getElementById("h1content");
+var h4content = document.getElementById("h4content");
+var question = document.getElementById("question");
+var intro = document.getElementById("intro");
+var quiz = document.getElementById("quiz");
+var quizQs = document.getElementById("quizQs");
+var feedback = document.getElementById("feedback");
+var initialsSubmit = document.getElementById("initialsSubmit");
+var initialsButton = document.getElementById("initialsButton");
+var initials = document.getElementById("initials");
+var goBack = document.getElementById("goBack");
+var correctAudio = document.getElementById("correctAudio");
+var incorrectAudio = document.getElementById("incorrectAudio");
+var crowd = document.getElementById("crowd");
+var questions = JSON.parse(localStorage.getItem("questions"));
+var score = 0;
+var i = 0;
+var timeLeft = 60;
+timer.textContent = "Time: 0";
+
+
+//Timing function
+function countdown() {
+  var timeInterval = setInterval(function () {
+    timer.textContent = "Time: " + timeLeft;
+    timeLeft--;
+
+    if (timeLeft === -1) {
+      timer.textContent = "Time: 0";
+      clearInterval(timeInterval);
+      gameOver();
+    }
+  }, 1000);
+  intro.classList.add("hidden");
+  quiz.classList.remove("hidden");
+  newQuestion();
+}
+
+
+
+//generate new question
+function newQuestion() {
+
+  if (i < questions.length) {
+    question.textContent = questions[i].title;
+    for (var j = 0; j < 4; j++) {
+      var answer = document.createElement("button");
+      answer.setAttribute("q-id", j + 1);
+      quizQs.append(answer);
+      answer.textContent = j + 1 + ". " + questions[i].choices[j];
+    }
+  }
+  else {
+    gameOver();
+  }
+}
+
+
+//code for when timer ends
+function gameOver() {
+  timeLeft = 0;
+  intro.classList.remove("hidden");
+  quiz.classList.add("hidden");
+  h1content.textContent = "The coding quiz is over!";
+  h4content.textContent = "Your final score was: " + score + " points";
+  startButton.classList.add("hidden");
+  initialsSubmit.classList.remove("hidden");
+  crowd.play();
+}
+
+
+
+//Event Listeners
+startButton.addEventListener("click", countdown);
+
+quizQs.addEventListener("click", function (event) {
+  if (event.target.matches("button")) {
+    event.preventDefault();
+    var userGuessId = event.target.getAttribute("q-id");
+
+    if (questions[i].choices[userGuessId - 1] == questions[i].answer) {
+      feedback.textContent = "Correct!";
+      feedback.classList.add("green");
+      feedback.classList.remove("red");
+      correctAudio.play();
+      score++;
+
+      while (quizQs.firstChild) {
+        quizQs.removeChild(quizQs.firstChild);
+      }
+      i++;
+      newQuestion();
+    }
+
+
+    else {
+      feedback.textContent = "Wrong!";
+      feedback.classList.add("red");
+      feedback.classList.remove("green");
+      incorrectAudio.play();
+      
+      while (quizQs.firstChild) {
+        quizQs.removeChild(quizQs.firstChild);
+      }
+      i++;
+      timeLeft = timeLeft - 10;
+      newQuestion();
+    }
+  }
+  else {
+    console.log("not a button");
+  }
+});
+
+
+//Submits initials text box to high scores list
+initialsButton.addEventListener("click", function (event) {
+event.preventDefault();
+if(initials.value.length > 3 || initials.value.length < 1) {
+  alert("Enter 3 or fewer characters to submit your initials")
+}
+else{
+  var newUser = {name: initials.value, score: score}; 
+  var allScores = JSON.parse(localStorage.getItem("allScores") || "[]");
+   
+  console.log(allScores);
+  allScores.push(newUser);
+  localStorage.setItem("allScores", JSON.stringify(allScores));
+  console.log(allScores);
+  window.open("highscores.html","_self");
+}
+});
+
+});
